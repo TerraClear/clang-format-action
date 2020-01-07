1026156571f425fce2c7312c836b834a345999e7
Paul Barsic <pbarsic@gmail.com>
2020-01-07 11:21:42 -0800

    some debug output

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
 echo "## Initializing git repo..."
 git init
 echo "### Adding git remote..."
+echo "### $REPO_FULLNAME"
 git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
 echo "### Getting branch"
 BRANCH=${GITHUB_REF#*refs/heads/}
@@ -23,6 +24,8 @@ git config --global user.name "TerraClear Dev"
 git update-index --assume-unchanged .github/workflows/*
 
 echo "## Running clang-format on C/C++ source"
+pwd
+ls
 SRC=$(git ls-tree --full-tree -r HEAD | grep -e "\.\(c\|h\|hpp\|cpp\)\$" | cut -f 2)
 
 clang-format -style=file -i $SRC
